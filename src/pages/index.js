2a09c47fb79c27eaa734a519264800ff12dea8f7
Samrat Jha <samratj@amazon.com>
2023-08-12 23:42:21 -0400

    feat: Initial setup of the gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,50 @@
+'use client'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useTransform, useScroll } from 'framer-motion'
+import { useRef } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
-
+const images = [
+  "1.jpeg",
+  "2.jpeg",
+  "3.jpeg",
+  "4.jpeg",
+  "5.jpeg",
+  "6.jpeg",
+  "7.jpeg",
+  "8.jpeg",
+  "9.jpeg",
+  "10.jpeg",
+  "11.jpeg",
+  "12.jpeg"
+]
 export default function Home() {
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen`}
     >
-     <div>
-      Hi There
+     <div className='h-[175vh] bg-[rgba(45,45,45)] flex gap-4 p-4 box-border'>
+        <Column images={[images[0], images[1], images[2]]} />
+        <Column images={[images[3], images[4], images[5]]} />
+        <Column images={[images[6], images[7], images[8]]} />
+        <Column images={[images[9], images[10], images[11]]} />
      </div>
     </main>
+  )
+}
+
+const Column = ({images}) => {
+  return (
+    <div className='relative w-[25%] h-full flex flex-col gap-2 min-w-[250px]'>
+      {
+        images.map((src, index) => {
+          return (
+            <div key={index} className='w-full h-full relative rounded-xl overflow-hidden'>
+              <Image src={`/${src}`} fill alt="image" className="object-cover" />
+            </div>
+          );
+        })
+      }
+    </div>
   )
 }
